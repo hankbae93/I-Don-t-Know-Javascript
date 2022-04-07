79e4a4e794e63f36920c15ac5f80a97fc337f8c4
ranja <seolin9340@gmail.com>
2022-04-07 20:00:26 +0900

    [Update] Ts TIL

--- zerocho-typescript/lecture.js
+++ zerocho-typescript/lecture.js
@@ -1,40 +1,17 @@
 "use strict";
 var imgCoords = "0";
 var rsp = {
+    // 딕셔너리 자료구조
     ROCK: "0",
     SCISSORS: "-142px",
     PAPER: "-284px"
-};
-var score = {
-    ROCK: 0,
-    SCISSORS: 1,
-    PAPER: -1
-};
-// imgCoords: "0" | "-142px" | "-284px"
+}; // as const도 설명
 function computerChoice(imgCoords) {
-    return Object.keys(rsp).find(function (k) { return rsp[k] === imgCoords; });
+    return Object.keys(rsp).find(function (k) {
+        return rsp[k] === imgCoords;
+    });
 }
 var interval;
-document.querySelectorAll(".btn").forEach(function (btn) {
-    btn.addEventListener("click", function (e) {
-        clearInterval(interval);
-        setTimeout(intervalMaker, 2000);
-        var myChoice = this.textContent;
-        var myScore = score[myChoice];
-        var computerScore = score[computerChoice(imgCoords)];
-        var diff = myScore - computerScore;
-        console.log(diff);
-        if (diff === 0) {
-            console.log("비김");
-        }
-        else if ([-1, 2].indexOf(diff)) {
-            console.log("이겻습니다");
-        }
-        else {
-            console.log("졋습니다.");
-        }
-    });
-});
 function intervalMaker() {
     interval = setInterval(function () {
         if (imgCoords === rsp.ROCK) {
@@ -46,9 +23,8 @@ function intervalMaker() {
         else {
             imgCoords = rsp.ROCK;
         }
-        var computer = document.querySelector("#computer");
-        if (computer) {
-            computer.style.background =
+        if (document.querySelector("#computer")) {
+            document.querySelector("#computer").style.background =
                 "url(https://en.pimg.jp/023/182/267/1/23182267.jpg) " +
                     imgCoords +
                     " 0";
@@ -56,3 +32,43 @@ function intervalMaker() {
     }, 100);
 }
 intervalMaker();
+var score = {
+    SCISSORS: 1,
+    ROCK: 0,
+    PAPER: -1
+};
+document.querySelectorAll(".btn").forEach(function (btn) {
+    btn.addEventListener("click", function () {
+        clearInterval(interval); // setInterval 중지
+        setTimeout(function () {
+            intervalMaker();
+        }, 1000);
+        var myChoice = this.id;
+        var myScore = score[myChoice];
+        var computerScore = score[computerChoice(imgCoords)];
+        var diff = myScore - computerScore;
+        if (diff === 0) {
+            console.log("비겼습니다");
+        }
+        else if ([-1, 2].includes(diff)) {
+            console.log("이겼습니다!!");
+        }
+        else {
+            console.log("졌습니다 ㅠㅠ.");
+        }
+    });
+});
+// SCISSORS: 1, ROCK: 0, PAPER: -1
+// 나\컴퓨터    SCISSORS   ROCK    PAPER
+//        SCISSORS   1 1    1 0   1 -1
+//        ROCK   0 1    0 0   0 -1
+//          PAPER  -1 1   -1 0  -1 -1
+var start = 3;
+var interval2 = setInterval(function () {
+    if (start === 0) {
+        console.log("종료!!!");
+        return clearInterval(interval2);
+    }
+    console.log(start);
+    start -= 1;
+}, 1000);
--- zerocho-typescript/lecture.ts
+++ zerocho-typescript/lecture.ts
@@ -1,56 +1,24 @@
-let imgCoords: RSP[keyof RSP] = "0";
-
 interface RSP {
-	readonly ROCK: "0";
-	readonly SCISSORS: "-142px";
-	readonly PAPER: "-284px";
+	ROCK: "0";
+	SCISSORS: "-142px";
+	PAPER: "-284px";
 }
 
+let imgCoords: RSP[keyof RSP] = "0";
 const rsp: RSP = {
+	// 딕셔너리 자료구조
 	ROCK: "0",
 	SCISSORS: "-142px",
 	PAPER: "-284px",
-};
+}; // as const도 설명
 
-interface Example {
-	a: 3;
-	b: 7;
-	[key: string]: number;
-}
-
-const score = {
-	ROCK: 0,
-	SCISSORS: 1,
-	PAPER: -1,
-};
-
-// imgCoords: "0" | "-142px" | "-284px"
-function computerChoice(imgCoords: RSP[keyof RSP]): keyof RSP {
-	return (Object.keys(rsp) as ["ROCK", "SCISSORS", "PAPER"]).find(
-		(k) => rsp[k] === imgCoords
-	)!;
-}
-let interval: number;
-document.querySelectorAll(".btn").forEach((btn) => {
-	btn.addEventListener("click", function (this: HTMLButtonElement, e: Event) {
-		clearInterval(interval);
-		setTimeout(intervalMaker, 2000);
-		const myChoice = this.textContent as keyof RSP;
-		const myScore = score[myChoice];
-		const computerScore = score[computerChoice(imgCoords)];
-		const diff = myScore - computerScore;
-		console.log(diff);
-
-		if (diff === 0) {
-			console.log("비김");
-		} else if ([-1, 2].indexOf(diff)) {
-			console.log("이겻습니다");
-		} else {
-			console.log("졋습니다.");
-		}
+function computerChoice(imgCoords: RSP[keyof RSP]) {
+	return (Object.keys(rsp) as ["ROCK", "SCISSORS", "PAPER"]).find((k) => {
+		return rsp[k] === imgCoords;
 	});
-});
+}
 
+let interval: number;
 function intervalMaker() {
 	interval = setInterval(function () {
 		if (imgCoords === rsp.ROCK) {
@@ -60,14 +28,55 @@ function intervalMaker() {
 		} else {
 			imgCoords = rsp.ROCK;
 		}
-
-		const computer = document.querySelector<HTMLDivElement>("#computer");
-		if (computer) {
-			computer.style.background =
+		if (document.querySelector("#computer")) {
+			(document.querySelector("#computer") as HTMLElement).style.background =
 				"url(https://en.pimg.jp/023/182/267/1/23182267.jpg) " +
 				imgCoords +
 				" 0";
 		}
 	}, 100);
 }
+
 intervalMaker();
+
+const score = {
+	SCISSORS: 1,
+	ROCK: 0,
+	PAPER: -1,
+};
+
+document.querySelectorAll(".btn").forEach((btn) => {
+	btn.addEventListener("click", function (this: HTMLButtonElement) {
+		clearInterval(interval); // setInterval 중지
+		setTimeout(() => {
+			intervalMaker();
+		}, 1000);
+		const myChoice = this.id as keyof RSP;
+		const myScore = score[myChoice];
+		const computerScore = score[computerChoice(imgCoords)!];
+		const diff = myScore - computerScore;
+		if (diff === 0) {
+			console.log("비겼습니다");
+		} else if ([-1, 2].includes(diff)) {
+			console.log("이겼습니다!!");
+		} else {
+			console.log("졌습니다 ㅠㅠ.");
+		}
+	});
+});
+
+// SCISSORS: 1, ROCK: 0, PAPER: -1
+// 나\컴퓨터    SCISSORS   ROCK    PAPER
+//        SCISSORS   1 1    1 0   1 -1
+//        ROCK   0 1    0 0   0 -1
+//          PAPER  -1 1   -1 0  -1 -1
+
+let start = 3;
+const interval2 = setInterval(function () {
+	if (start === 0) {
+		console.log("종료!!!");
+		return clearInterval(interval2);
+	}
+	console.log(start);
+	start -= 1;
+}, 1000);
